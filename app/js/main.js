b49873e5e4b0f1ce3379e1c038060a98def1af8b
StanislavchukMikhailo24 <mihasstas@gmail.com>
2020-02-12 11:43:57 +0200

    addet product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,17 @@ $(function(){
 
   var mixer = mixitup('.products__inner-box'); 
 
+  $(".rate-star").rateYo({
+    rating: 3.6,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $(".product-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 
 });
