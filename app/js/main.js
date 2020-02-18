190a1a507100f3b7deb65b68a1f125d48405ddc9
StanislavchukMikhailo24 <mihasstas@gmail.com>
2020-02-18 16:32:02 +0200

    new items+filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 $(function(){
 
-  var mixer = mixitup('.products__inner-box'); 
-
+  
   $(".rate-star").rateYo({
     rating: 3.6,
     starWidth: "12px",
@@ -14,5 +13,35 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+  $(".js-range-slider").ionRangeSlider({
+
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+
+});
+
+$('.icon-th-list').on('click', function(){
+  $('.products__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large ').removeClass('active');
+});
+
+$('.icon-th-large ').on('click', function(){
+  $('.products__item').removeClass('list');
+  $('.icon-th-large ').addClass('active');
+  $('.icon-th-list').removeClass('active');
+});
+
+if ($('.products__inner-box').length) {
+  var mixer = mixitup('.products__inner-box');
+}
+
+
+
 
 });
