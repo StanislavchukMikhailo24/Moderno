3864d596e10bc3eaca48b37fa3a542d6f0e6de50
StanislavchukMikhailo24 <mihasstas@gmail.com>
2020-02-21 09:01:06 +0200

    adaptive footer and header

--- app/js/main.js
+++ app/js/main.js
@@ -26,16 +26,28 @@ $(function(){
 });
 
 $('.icon-th-list').on('click', function(){
-  $('.products__item').addClass('list');
+  $('.product-page__items .products__item').addClass('list');
   $('.icon-th-list').addClass('active');
   $('.icon-th-large ').removeClass('active');
 });
 
 $('.icon-th-large ').on('click', function(){
-  $('.products__item').removeClass('list');
+  $('.product-page__items .products__item').removeClass('list');
   $('.icon-th-large ').addClass('active');
   $('.icon-th-list').removeClass('active');
 });
+
+
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+
 
 if ($('.products__inner-box').length) {
   var mixer = mixitup('.products__inner-box');
